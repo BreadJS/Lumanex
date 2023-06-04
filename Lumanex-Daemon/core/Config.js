c2d83b7f800f8fe14448c661237dcf1cd0c33fc3
BreadJS <devvcat@protonmail.com>
2023-06-04 23:09:21 +0200

    [Config] Created Config.js

---
+++ Lumanex-Daemon/core/Config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /* Do not touch these values, they will be filled in by the code */
+  BLOCKCHAIN_FOLDER: "",
+
+  COINNAME: "Lumanex",
+  TICKER: "LNX",
+  VERSION: "1.0.0",
+  P2P_PORT: 23000,
+  RPC_PORT: 23001,
+};
