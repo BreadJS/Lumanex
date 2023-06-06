64ab843c6649d3462d4a1015dd5c937080cdfafe
BreadJS <devvcat@protonmail.com>
2023-06-06 06:04:16 +0200

    [Core] Created 'Core.js'

---
+++ Lumanex-Daemon/Core/Core.js
@@ -0,0 +1,24 @@
+const commandLineArgs = require('command-line-args');
+
+function generateNodeID() {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const idLength = 32;
+  let id = '';
+
+  for (let i = 0; i < idLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    id += characters.charAt(randomIndex);
+  }
+
+  return id;
+}
+
+module.exports = {
+  cmdArgs: commandLineArgs([
+    { name: 'p2p-port', type: Number },
+    { name: 'rpc-port', type: Number }
+  ]),
+  
+  nodeID: generateNodeID(),
+  connectedPeers: 0
+};
